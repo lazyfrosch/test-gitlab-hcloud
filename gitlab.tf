8fbd6ed1c42dfece4959c295a5ace1f3cfb9acde
Markus Frosch <markus@lazyfrosch.de>
2022-01-04 09:15:38 +0100

    Add root password

--- gitlab.tf
+++ gitlab.tf
@@ -1,3 +1,8 @@
+variable "gitlab_root_password" {
+  sensitive = true
+  default   = ""
+}
+
 resource "hcloud_server" "gitlab" {
   name        = "gittest.lazyfrosch.de"
   image       = "debian-11"
@@ -5,8 +10,15 @@ resource "hcloud_server" "gitlab" {
   server_type = "cx21"
   ssh_keys    = [data.hcloud_ssh_key.admin.id]
 
+  lifecycle {
+    ignore_changes = [
+      user_data
+    ]
+  }
+
   user_data = templatefile("user-data.yml", {
-    gitlab_fqdn = "gittest.lazyfrosch.de",
+    gitlab_fqdn          = "gittest.lazyfrosch.de",
+    gitlab_root_password = var.gitlab_root_password,
   })
 }
 
@@ -30,4 +42,20 @@ resource "hcloud_rdns" "gitlab" {
   server_id  = hcloud_server.gitlab.id
   ip_address = hcloud_server.gitlab.ipv4_address
   dns_ptr    = "gittest.lazyfrosch.de"
+}
+
+resource "hetznerdns_record" "registrytest" {
+  zone_id = data.hetznerdns_zone.zone.id
+  name    = "registrytest" # registrytest.lazyfrosch.de
+  value   = hcloud_server.gitlab.ipv4_address
+  type    = "A"
+  ttl     = 300
+}
+
+resource "hetznerdns_record" "registrytest6" {
+  zone_id = data.hetznerdns_zone.zone.id
+  name    = "registrytest" # registrytest.lazyfrosch.de
+  value   = hcloud_server.gitlab.ipv6_address
+  type    = "AAAA"
+  ttl     = 300
 }
